0bf6831cde9428742b78e3a1740a58c55ff10b7d
ArtyomFadeev774 <artyom.fadeev.07@list.ru>
2023-05-21 12:18:23 +0500

    добавил файл для работы с валютами, сделал заготовку в главном файле.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import os
 from dotenv import load_dotenv
+import telebot
+from telebot import types
+from valute import *
+
 
 load_dotenv()
-
 token = os.getenv('TOKEN')
-print(token)
+
+bot = telebot.TeleBot(token)
+
+def create_keyboard():
+    keyboard = types.ReplyKeyboardMarkup()
+    buttons = create_buttons()
+    for button in buttons:
+        btn = types.KeyboardButton(button)
+        keyboard.add(btn)
+    return keyboard
+
+@bot.message_handler(commands=['start', 'go'])
+def start(message):
+    #print(message)
+    bot.send_message(message.from_user.id, 'Привет', reply_markup=create_keyboard())
+
+@bot.message_handler(content_types=['text'])
+def go(message):
+    bot.send_message(message.from_user.id, message.text)
+
+
+
+print('Бот Работает!')
+bot.polling(non_stop=True, interval=0)
---
+++ valute.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def create_request():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    result = requests.get(url).json()
+    return result
+
+def create_buttons():
+    res = create_request()
+    results = []
+    for element in res['Valute']:
+        results.append(element)
+    return results
+
+def get_data(key):
+    res = create_request()
+    return str(res['Valute'][key]['Value'])
